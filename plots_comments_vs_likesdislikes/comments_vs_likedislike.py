fb9a92a5eda5dbfcb255f56c5e0475f6350801be
BHUSHAN KULKARNI <bhushankulkarni117@gmail.com>
2015-09-25 17:38:32 +0530

    pearson added

--- plots_comments_vs_likesdislikes/comments_vs_likedislike.py
+++ plots_comments_vs_likesdislikes/comments_vs_likedislike.py
@@ -35,31 +35,9 @@ def comments_vs_likedislike(working_directory):
 	normalised_ratio=[(float(likes[i]+1)*100.0/(float(likes[i]+1)+float(dislikes[i]+1))) for i in range(len(likes))]
 	plot(normalised_ratio,comments)
 
-
-	"""top1000=open(working_directory+'MyCollection/top_1000id.txt','r')
-	top1000id=top1000.read().split('\n')[:-1]
-	print top1000id[0]
-	top1000.close()
-	top10000=open(working_directory+'MyCollection/top_10000id.txt','r')
-	top10000id=top10000.read().split('\n')[:-1]
-	top10000.close()
-	datadirectory=working_directory+'obtainedFor91000/Scatter/'
-	like=open(datadirectory+'viewcount_likes.dat','r')
-	dislike=open(datadirectory+'viewcount_dislikes.dat','r')
-	comment=open(datadirectory+'viewcount_comment.dat','r')
-	likes=like.read().split('\n')[:-1]
-	print likes[0].split('\t')
-	dislikes=dislike.read().split('\n')[:-1]
-	comments=comment.read().split('\n')[:-1]
-	like.close()
-	dislike.close()
-	comment.close()"""
-	
-
 def main():
 	working_directory='/home/bhushan/Documents/Soc_Comp_Project/youtube_data/'
 	comments_vs_likedislike(working_directory)
 
 if __name__=="__main__":
 	main()
-
