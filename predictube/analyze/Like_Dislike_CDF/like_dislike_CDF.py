ebe4c5203b425745c917628a7ee4634036d82acc
BHUSHAN KULKARNI <bhushankulkarni117@gmail.com>
2015-10-02 12:01:31 +0530

    Like_Dislike_CDF updated

--- predictube/analyze/Like_Dislike_CDF/like_dislike_CDF.py
+++ predictube/analyze/Like_Dislike_CDF/like_dislike_CDF.py
@@ -11,18 +11,32 @@ def getCDF(numbers):
 	for j in numbers:
 		for k in range(j,len(out)):
 			out[k]=out[k]+1
-	return out
+	mx=max(out)
+	return [i/float(mx) for i in out]
 
 def plotCombinedCDF(Xs,labels,tit):
 	try:
 		Xs.remove([])
 	except:
 		pass
-	plt.title(tit)
+	plt.figure()
+	plt.title('CDF of likes,dislikes for category: '+tit)
+	plt.ylabel('CDF')
 	for X in range(len(Xs)):
 		plt.plot([float(i)/len(Xs[X]) for i in range(len(Xs[X]))],Xs[X],label=labels[X])
 	plt.legend()
-	plt.show()
+	plt.savefig('CDF of likes,dislikes for category: '+tit+'.png')
+
+def plotIndividual(Y,label,tit):
+	if len(Y)==0:
+		return
+	plt.figure()
+	X=[i for i in range(len(Y))]
+	plt.title('CDF of '+label+' for category: '+tit)
+	plt.ylabel('CDF')
+	plt.xlabel(label)
+	plt.plot(X,Y)
+	plt.savefig('CDF of '+label+' for category: '+tit+'.png')
 
 def main():
 	f=open(working_directory+'MasterData.csv','r')
@@ -35,7 +49,9 @@ def main():
 		likes=[int(ids[i].split(',')[2]) for i in range(len(ids)) if ids[i].split(',')[6]==cat]
 		dislikes=[int(ids[i].split(',')[3]) for i in range(len(ids)) if ids[i].split(',')[6]==cat]
 		likeCDF=getCDF(likes)
+		plotIndividual(likeCDF,'likes',cat)
 		dislikeCDF=getCDF(dislikes)
+		plotIndividual(dislikeCDF,'dislikes',cat)
 		labels=['like','dislike']
 		plotCombinedCDF([likeCDF,dislikeCDF],labels,cat)
 
